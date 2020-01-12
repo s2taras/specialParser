ce1217e1cd2360f5c9168930bf01c01c236eccde
Lytvyn <lytvyn.sbit@gmail.com>
2020-01-12 21:22:55 +0200

    add xdebug for cli, create env file for data parsing, download simpleHtmlDom

--- parser/index.php
+++ parser/index.php
@@ -1,4 +1,24 @@
 <?php
 
-echo "hello from terminal";
-echo "\n";
+require_once './vendor/autoload.php';
+require_once "simple_html_dom.php";
+
+use Parser\ExcludedCategories;
+
+$html = new simple_html_dom();
+
+// Get main page
+$html->load_file(getenv("MAIN_URL"));
+
+// Select categories
+$links = $html->find('.sidebar-menu__link');
+
+// Exclude unnecessary categories
+$excludedCategories = ExcludedCategories::getExcludedCategories();
+$newListCategories = [];
+
+foreach ($links as $linkCategory) {
+    if (!array_key_exists($linkCategory->attr['href'], $excludedCategories)) {
+        $newListCategories[] = $linkCategory;
+    }
+}
---
+++ parser/src/ExcludedCategories.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Parser;
+
+class ExcludedCategories
+{
+    public static function getExcludedCategories(): array
+    {
+        return [
+            '/g7022977-masla-dlya-kutikuly' => 'Масла для кутикулы',
+            '/g10748170-parikmaherskie-vorotnichki-dlya' => 'Парикмахерские воротнички для стрижки и пеньюары',
+            '/g6346745-kejsy-sumochki-chemodany' => 'Кейсы, сумочки, чемоданы, косметички органайзеры для профессионального и частного использования',
+            '/g6177474-gel-laki-bling' => 'Гель лаки Bling мини. Серия для профессионального и частного использования',
+            '/g6166952-emi-emi-gel' => 'EMI Еми- гель краски / всё для дизайна ногтей',
+            '/g6681620-ukrasheniya-dlya-devushek' => 'Украшения для девушек',
+            '/g29553627-produktsiya-cnd' => 'Продукция CND',
+            '/g81223921-pustaya-tara-dlya' => 'Пустая тара для косметики',
+            '/g82319400-gel-laki-lover' => 'Гель лаки LoveR',
+            '/g6139087-gel-laki-you' => 'Гель лаки YOU POSH',
+            '/g10350648-flash-tatoo-flesh' => 'Flash TATOO флэш тату! Кружева черные и белые для тела',
+            '/g10677907-vannochki-dlya-manikyura' => 'Ванночки для маникюра',
+            '/g8653404-kisti-dlya-dizajna' => 'Кисти для дизайна, моделирования и росписи ногтей',
+            '/g7117222-pilochki-bafiki-evropemza' => 'Пилочки, бафики, европемза, блоки, бруски для маникюра',
+            '/g8577285-cnd-creative-ssha' => 'CND-Creative (США) официальное представительство!',
+            '/g6701874-depilyatsiya-italiya-ispaniya' => 'Депиляция (Италия, Испания)',
+            '/g6399630-professionalnye-zhidkosti-dlya' => 'Профессиональные жидкости для наращивания ногтей и маникюра',
+            '/g20119073-kamuflyazhnaya-baza-you' => 'Камуфляжная База you-POSH French Rubber Base',
+            '/g6056390-slajd-dizajn-vodnye' => 'Слайд дизайн, водные наклейки, слайдер дизайны',
+            '/g6994907-naraschivanie-resnits-materialy' => 'Наращивание ресниц, материалы и аксессуары для наращивания ресниц',
+            '/g9235882-yuvelirnye-ukrasheniya-dlya' => 'Ювелирные украшения для ногтей, 3Д украшения для ногтей',
+            '/g10653426-strazy-krisally-fianity' => 'Стразы, крисаллы, фианиты для дизайна ногтей',
+            '/g10687358-dekor-dlya-dizajna' => 'Декор для дизайна ногтей в карусельках',
+        ];
+    }
+}
